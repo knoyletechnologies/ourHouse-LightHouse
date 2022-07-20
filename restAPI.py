f3255a4d105e26019f0764f34e1fdac90ec243be
Thomas <t.w.youens2@newcastle.ac.uk>
2022-07-20 15:34:36 +0100

    updated api header keys

--- restAPI.py
+++ restAPI.py
@@ -18,26 +18,20 @@ class API:
         else:
             return response.content
 
-    def house(self, house, key):
-        headers = {'light-house-key': key, 'light-house-house': house}
+    def house(self, house, key, node):
+        headers = {'X-LIGHT-HOUSE-KEY': key, 'X-LIGHT-HOUSE-NODE': node}
         response = requests.get(baseURL + "/v1" + "/house/" + house, headers)
         if response.status_code == 200:
             return response.content
         else:
             return {}
 
-    def presenceHosts(self, house, key):
-        headers = {'light-house-key': key, 'light-house-house': house}
-        response = requests.get(baseURL + "/v1" + "/house/" + house + "/lightHouse/presence/hosts", headers)
-        if response.status_code == 200:
-            return response.content
-        else:
-            return {}
+    def presenceHosts(self, house, key, node):
+        headers = {'X-LIGHT-HOUSE-KEY': key, 'X-LIGHT-HOUSE-NODE': node}
+        response = requests.get(baseURL + "/v1" + "/house/" + house + "/lightHouse/presence/hosts", headers=headers)
+        return response.content
 
-    def presenceUpdate(self, house, key, data):
-        headers = {'light-house-key': key, 'light-house-house': house}
-        response = requests.post(baseURL + "/v1" + "/house/" + house + "/lightHouse/presence/update", headers, data)
-        if response.status_code == 200:
-            return response.content
-        else:
-            return {}
+    def presenceUpdate(self, house, key, node, data):
+        headers = {'X-LIGHT-HOUSE-KEY': key, 'X-LIGHT-HOUSE-NODE': node}
+        response = requests.post(baseURL + "/v1" + "/house/" + house + "/lightHouse/presence/update", headers=headers, json=data)
+        return response.content
